ad928251665b3dfdc8020db9721da0465403ce7e
spinner99-bit <121241376+spinner99-bit@users.noreply.github.com>
2025-06-15 19:52:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 const redirectTo = 'https://web.lg-gaming.xyz';
 
 app.use((req, res) => {
   res.redirect(redirectTo);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running and redirecting to ${redirectTo}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT} and redirecting to ${redirectTo}`);
 });
